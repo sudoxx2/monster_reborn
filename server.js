f6f96105557608bcab136df4208f85259500418a
sudoxx2 <sudoxx2@gmail.com>
2019-12-11 23:40:05 -0800

    served static files with express

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
+const path = require('path');
 
 const app = express()
 
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-    res.render('index')
-})
+// app.use(express.static('template'))
+
+app.use(express.static("template"));
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/static/main.css'));
+// })
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/template/base.html'));
+// })
 
 app.listen(5656, () => {
     console.log('http://localhost:5656')
