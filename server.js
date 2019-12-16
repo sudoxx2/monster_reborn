6f5bda854f35e7ebc98170a37bb30c9b21d07494
sudoxx2 <sudoxx2@gmail.com>
2019-12-16 00:02:41 -0800

    handlebar success initial layout

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const path = require('path');
+const expbs = require('express-handlebars');
 
 const app = express()
+
+app.engine('handlebars', expbs());
+app.set('view engine', 'handlebars');
+
+
 
 // app.set('view engine', 'pug');
 
@@ -9,17 +15,17 @@ const app = express()
 
 // app.use(express.static("template"));
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/static/main.css'));
-// })
+app.get('/', (req, res) => {
+    res.render('index')
+})
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname + '/template/base.html'));
 // })
 
-app.get('/',function(req,res) {
-    res.sendFile(__dirname + '/template/base.html');
-  });
+// app.get('/',function(req,res) {
+//     res.sendFile(__dirname + '/template/base.html');
+//   });
 
 app.listen(3000, () => {
     console.log('http://localhost:3000')
