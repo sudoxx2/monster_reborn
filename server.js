8c55267b61fdbe18419e5c140483e39364f345a6
sudoxx2 <sudoxx2@gmail.com>
2019-12-21 04:16:23 -0800

    added port and proc file for deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const PORT = process.env.port || 3000
 const express = require("express");
 const path = require('path');
 const expbs = require('express-handlebars');
@@ -57,5 +58,5 @@ app.get('/mortgage-loan-calc', function(req, res) {
 });
 
 app.listen(3000, () => {
-    console.log('http://localhost:3000')
+    console.log('http://localhost:' + PORT)
 })
